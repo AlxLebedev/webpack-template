1152e63a40a733eccf827b3b7880a5198712ede5
Alexey Lebedev <vorresti@ya.ru>
2020-07-12 09:21:14 +0300

    swiper to module

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
 import './scss/style.scss';
 import './css/css-style.css';
 import './js/app';
-
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-// import Swiper styles
-import 'swiper/swiper-bundle.css';
-
-// configure Swiper to use modules
-Swiper.use([Navigation, Pagination]);
-
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-})
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 import add from './Modules/add';
+import Swiper from './Modules/Swiper'
 
 console.log(add(5, 5));
